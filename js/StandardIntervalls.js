628e2ac5e341eb22b4ad79f9baccabe17b6347b8
Joakim Lehn <joakim.lehn@knowit.no>
2016-09-20 20:20:44 +0200

    sise opprydding

--- js/StandardIntervalls.js
+++ js/StandardIntervalls.js
@@ -4,6 +4,9 @@ var standardIntervalls = Observable();
 
 standardIntervalls.add({name:"4x4", subtext:"Completed 0 times"})
 standardIntervalls.add({name:"6x6", subtext:"Completed 5 times"})
+standardIntervalls.add({name:"20x1", subtext:"Completed 5 times"})
+standardIntervalls.add({name:"Tabata", subtext:"Completed 5 times"})
+standardIntervalls.add({name:"Håkons spesial", subtext:"Completed 5 times"})
 
 module.exports = {
 	standardIntervalls: standardIntervalls
